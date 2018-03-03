7c713163786dce473c27ed2cbe87e0c5a8b4bf3f
Will Bender <willypbender83@gmail.com>
2018-03-03 14:01:17 -0600

    Fix: Make link-provider divide line into fields based on spaces.

--- src/link-provider.ts
+++ src/link-provider.ts
@@ -3,6 +3,15 @@ import * as vscode from 'vscode';
 import { getConfiguration } from './configuration';
 import { Rule } from './rule';
 
+class Field {
+    value: string;
+    pos: number;
+    constructor(value: string, pos: number) {
+        this.value = value;
+        this.pos = pos;
+    }
+}
+
 export class LinkProvider implements vscode.DocumentLinkProvider {
     private get fieldsOnly(): boolean { 
         return getConfiguration().fieldsOnly;
@@ -20,38 +29,56 @@ export class LinkProvider implements vscode.DocumentLinkProvider {
     }
 
     private getMatchesOnLine(rules: Rule[], line: string, lineNo: number, fieldsOnly: boolean, matches: vscode.DocumentLink[]): vscode.DocumentLink[] {
+        var fields:Field[];
+
+        if (fieldsOnly) {
+            var searchPos = 0;
+            var fieldValues = line.match(/[^ ]+/g) || [];
+            fields = fieldValues.map(value => {
+                var pos = line.indexOf(value, searchPos);
+                searchPos = pos + value.length;
+                return new Field(value, pos);
+            });
+        } else {
+            fields = [ new Field(line, 0) ];
+        }
+        
         rules.forEach(rule => {
-            const regexString = (fieldsOnly) ? `(?:^| )${rule.match}(?: |$)` : `${rule.match}`;
+            const regexString = rule.match;
             const expr = new RegExp(regexString, 'gi');
-            let match;
-            while (true) {
-                match = expr.exec(line);
-                if (match === null) {
-                    break;
+            fields.forEach(field => {
+                let match;
+                while(true) {
+                    match = expr.exec(field.value);
+                    if (match === null) {
+                        break;
+                    }
+                    if (match !== null) {
+                        //Set character index range for link
+                        var start = field.pos + match.index;
+                        var end = field.pos + match.index + match[0].length;
+
+                        start = match[0].startsWith(" ") ? start + 1 : start;
+                        end = match[0].endsWith(" ") ? end - 1 : end;
+        
+                        const range = new vscode.Range(
+                            new vscode.Position(lineNo, start),
+                            new vscode.Position(lineNo, end)
+                        );
+        
+                        //Form urlstring
+                        var url = rule.result;
+                        for (var i:number = 1; i < match.length; i++) {
+                            url = url.replace(`\\${i}`, match[i]);
+                        }
+        
+                        matches.push({
+                            range,
+                            target: vscode.Uri.parse(url)
+                        });
+                    }
                 }
-
-                //Set character index range for link
-                var start = match.index;
-                var end = match.index + match[0].length;
-                //If captured spaces
-                start = match[0].startsWith(" ") ? start + 1 : start;   
-                end = match[0].endsWith(" ") ? end - 1 : end;
-                const range = new vscode.Range(
-                    new vscode.Position(lineNo, start),
-                    new vscode.Position(lineNo, end)
-                );
-
-                //Form urlstring
-                var url = rule.result;
-                for (var i:number = 1; i < match.length; i++) {
-                    url = url.replace(`\\${i}`, match[i]);
-                }
-
-                matches.push({
-                    range,
-                    target: vscode.Uri.parse(url)
-                });
-            }
+            });
         });
         return matches;
     }

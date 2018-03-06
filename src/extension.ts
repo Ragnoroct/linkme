5070c36d45bfb734e1022e0f360785883215a3bb
Will Bender <willypbender83@gmail.com>
2018-03-05 21:47:46 -0600

    Dispose of linkprovider when deactivated

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,8 @@ import { getConfiguration } from './configuration';
 
 // this method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-    const linkProvider = new LinkProvider(getConfiguration());
-    vscode.languages.registerDocumentLinkProvider('*', linkProvider);
-}
+    const linkProvider = new LinkProvider(getConfiguration);
+    var disposable = vscode.languages.registerDocumentLinkProvider('*', linkProvider);
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-
+    context.subscriptions.push(disposable);
 }
